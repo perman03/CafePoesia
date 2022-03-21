87d5306f75f9f1f51a75aafadd87172d7fe0b560
Franco Perez <frankperman03@gmail.com>
2022-03-21 14:22:23 -0600

    minifying images with imagemin7.1.0 and adding issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+//Dependencias para CSS
 const sass = require('gulp-sass') (require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer'); 
+//Dependencias para minificar imagenes
+const imagemin = require('gulp-imagemin');
 
 // Declarar tarea
 function css (done) {
@@ -17,19 +20,34 @@ function css (done) {
     
 }
 
-//controlador autoguardado
+//tarea para pasar las imagenes al build
+function imagenes(done){
+
+
+    src('src/img/**/*')                                           //asterisco = todos los archivos y sus extension
+        .pipe( imagemin({optimizationLevel: 3}))                  //tuberia para minificar imagenes
+        .pipe( dest('build/img') )
+
+    
+    done(); 
+}
+
+//tarea controlador autoguardado
 function dev(){
     watch('src/scss/**/*.scss', css);          //permite compilar todos los archivos con terminacion scss
+    watch('src/img/**/*', imagenes); 
 }
 
 function tareaDefault(){
     console.log('soy una tarea default');   
 }
 
-//Permitir mandar a llamar tarea
+
+//Permitir llamado de tarea
 exports.css = css; 
 exports.dev = dev; 
-exports.default = series(css, dev); 
+exports.default = series(imagenes, css, dev);
+exports.imagenes = imagenes; 
 
 //series - Se inicia una tarea y hasta que finaliza se inicia otra
 //parallel - Todas inician al mismo tiempo
